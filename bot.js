de571e4f81384c9ca5165b6e905409b3c7b97592
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-20 14:50:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,28 +27,33 @@ client.on('ready', () => {
   console.log('')
 });
 
-client.on("message", message => {
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
 
-          var args = message.content.substring(prefix.length).split(" ");
-          if (message.content.startsWith(prefix + "clear")) {
- if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('  ⚠  ** **');
-      var msg;
-      msg = parseInt();
-
-    message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-    message.channel.sendMessage("", {embed: {
-      title: "Done | تــم",
-      color: 0x06DF00,
-      description: "تم مسح الرسائل بنجاح",
-      footer: {
-        text: ""
-      }
-    }}).then(msg => {msg.delete(3000)});
-                        }
-
-
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***
+100 :ok_hand:
+***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("
+php\nعدد الرسائل التي تم مسحها: " + textxt + "\n
+").then(m => m.delete(3000));
+        }    
+    }
+}
 });
-  
 
 
 client.login(process.env.BOT_TOKEN);
