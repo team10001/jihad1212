06f18bfda1179800dad63ae3be552163be64687c
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-20 12:57:44 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const fs = require("fs");
+
+const Canvas = require("canvas");
+
+const jimp = require("jimp");
+
+const moment = require("moment")
+
+
+client.on('ready', () => {
+
+  console.log(`Logged in as ${client.user.tag}!`);
+
+client.user.setGame(`*help | *invite `,"http://twitch.tv/S-F")
+
+  console.log('')
+
+  console.log('')
+
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+
+  console.log(`[Start] ${new Date()}`);
+
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+
+  console.log('')
+
+  console.log('╔[════════════════════════════════════]╗');
+
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+
+  console.log('')
+
+  console.log('Informations :')
+
+  console.log('')
+
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+
+  console.log(`Users! [ " ${client.users.size} " ]`);
+
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+
+  console.log('╚[════════════════════════════════════]╝')
+
+  console.log('')
+  console.log('╔[════════════]╗')
+
+  console.log(' Bot Is Online')
+
+  console.log('╚[════════════]╝')
+
+  console.log('')
+
+  console.log('')
+
+});
