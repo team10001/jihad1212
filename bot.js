1fca2639c04d6f4077bda7a8590194b6050e87be
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-20 13:45:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,38 +59,27 @@ client.on('ready', () => {
 
 });
 
- KiNg66S.on('message', function(KiNg66S) {
-if (KiNg66S.author.bot) return;
-if (KiNg66S.author.id === KiNg66S.user.id) return;
-if (KiNg66S.author.equals(KiNg66S.user)) return;
-if (!KiNg66S.content.startsWith(prefix)) return;
+client.on("message", message => {
+  var prefix = "^";
 
-var args = KiNg66S.content.substring(prefix.length).split(' ');
-switch (args[0].toLocaleLowerCase()) {
-case "مسح" :
-KiNg66S.delete()
-if(!KiNg66S.channel.guild) return
-if(KiNg66S.member.hasPermissions(0x2000)){ if (!args[1]) {
-KiNg66S.channel.fetchMessages()
-.then(messages => {
-KiNg66S.channel.bulkDelete(messages);
-var messagesDeleted = messages.array().length;
-KiNg66S.channel.sendMessage(' '+ " " + messagesDeleted + " " +  '**: عدد الرسائل التي تم مسحه**').then(m => m.delete(2500));
-})
-} else {
-let messagecount = parseInt(args[1]);
-KiNg66S.channel.fetchMessages({limit: messagecount}).then(messages => KiNg66S.channel.bulkDelete(messages));
-KiNg66S.channel.sendMessage(' '+ " " + args[1] + " " +  '**: عدد الرسائل التي تم مسحه**').then(m => m.delete(2500));
-KiNg66S.delete(60000);
-}
-} else {
-var manage = new Discord.RichEmbed()
-.setDescription('You Do Not Have Permission MANAGE_MESSAGES :(')
-.setColor("RANDOM")
-KiNg66S.channel.sendEmbed(manage)
-return;
-}
-}
+          var args = message.content.substring(prefix.length).split(" ");
+          if (message.content.startsWith(prefix + "clear")) {
+ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('  ⚠  ** **');
+      var msg;
+      msg = parseInt();
+
+    message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+    message.channel.sendMessage("", {embed: {
+      title: "Done | تــم",
+      color: 0x06DF00,
+      description: "تم مسح الرسائل بنجاح",
+      footer: {
+        text: ""
+      }
+    }}).then(msg => {msg.delete(3000)});
+                        }
+
+
 });
 
 client.login(process.env.BOT_TOKEN);
