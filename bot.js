607f3aabc9eb55bf010053e244fe6343946012f1
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-20 13:24:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,6 @@ client.on('ready', () => {
 
   console.log(`Logged in as ${client.user.tag}!`);
 
-client.user.setGame(`*help | *invite `,"http://twitch.tv/S-F")
-
   console.log('')
 
   console.log('')
@@ -58,6 +56,34 @@ client.user.setGame(`*help | *invite `,"http://twitch.tv/S-F")
   console.log('')
 
   console.log('')
+
+});
+
+client.on('message', message => {
+     if(message.content.startsWith(prefix + "clear")) {
+         var args = message.content.split(" ").slice(1);
+ if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You need MANAGE_MESSAGES permission noob');
+  if (!args[0]) return message.channel.send('You didn't provide any number!!!');
+
+  message.channel.bulkDelete(args[0]).then(() => {
+    const embed = new Discord.RichEmbed()
+      .setColor(0xF16104)
+      .setDescription(Cleared ${args[0]} messages.);
+    message.channel.send({ embed });
+
+    const actionlog = message.guild.channels.find('name', 'logs');
+
+    if (!actionlog) return message.channel.send('Can't find action-log channel. Are you sure that this channel exists and I have permission to view it? CANNOT POST LOG.');
+    const embedlog = new Discord.RichEmbed()
+      .setDescription('~Purge~')
+      .setColor(0xF16104)
+      .addField('Purged By', <@${message.author.id}> with ID ${message.author.id})
+      .addField('Purged in', message.channel)
+      .addField('Time', message.createdAt);
+    actionlog.send(embedlog);
+
+  });
+};
 
 });
 
