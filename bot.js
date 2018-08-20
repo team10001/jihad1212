5a70c0dd9bc888a3b46bf911bff19442fd18875c
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-20 17:32:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,33 +27,5 @@ client.on('ready', () => {
   console.log('')
 });
 
-client.on('message', msg => {
-  if (msg.author.bot) return;
-  if (!msg.content.startsWith(prefix)) return;
-  let command = msg.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = msg.content.split(" ").slice(1);
-
-    if(command === "clear") {
-        const emoji = client.emojis.find("name", "wastebasket")
-    let textxt = args.slice(0).join("");
-    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-    if (textxt == "") {
-        msg.delete().then
-    msg.channel.send("***
-100 :ok_hand:
-***").then(m => m.delete(3000));
-} else {
-    msg.delete().then
-    msg.delete().then
-    msg.channel.bulkDelete(textxt);
-        msg.channel.send("
-php\nعدد الرسائل التي تم مسحها: " + textxt + "\n
-").then(m => m.delete(3000));
-        }    
-    }
-}
-});
-
 
 client.login(process.env.BOT_TOKEN);
