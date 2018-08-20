00c5b007b93967647017aca396fb705b147a41bd
team10001 <41784577+team10001@users.noreply.github.com>
2018-08-20 13:52:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,66 +1,34 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-const fs = require("fs");
-
-const Canvas = require("canvas");
-
-const jimp = require("jimp");
-
-const moment = require("moment")
-
-const prefix = '+';
+const prefix = '+'
 
 client.on('ready', () => {
-
   console.log(`Logged in as ${client.user.tag}!`);
-
   console.log('')
-
   console.log('')
-
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-
   console.log(`[Start] ${new Date()}`);
-
   console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-
   console.log('')
-
   console.log('╔[════════════════════════════════════]╗');
-
   console.log(`Logged in as * [ " ${client.user.username} " ]`);
-
   console.log('')
-
   console.log('Informations :')
-
   console.log('')
-
   console.log(`servers! [ " ${client.guilds.size} " ]`);
-
   console.log(`Users! [ " ${client.users.size} " ]`);
-
   console.log(`channels! [ " ${client.channels.size} " ]`);
-
   console.log('╚[════════════════════════════════════]╝')
-
   console.log('')
   console.log('╔[════════════]╗')
-
   console.log(' Bot Is Online')
-
   console.log('╚[════════════]╝')
-
   console.log('')
-
   console.log('')
-
 });
 
 client.on("message", message => {
-  
+
           var args = message.content.substring(prefix.length).split(" ");
           if (message.content.startsWith(prefix + "clear")) {
  if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('  ⚠  ** **');
@@ -80,5 +48,7 @@ client.on("message", message => {
 
 
 });
+  
+
 
 client.login(process.env.BOT_TOKEN);
